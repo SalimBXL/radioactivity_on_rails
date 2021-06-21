8c9378fa88c61123e89f266fb265f0be077d7508
Salim A. JOLY <el.salim.salim@gmail.com>
2021-06-21 11:01:59 +0200

    page de garde

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+clients = Client.create([
+        { name: '- - -' }, 
+        { name: 'Bordet'}
+    ])
+
+tracers = Tracer.create([
+        { name: 'FDG', molecule: 'F18', halflife: '109.77', color: 'green'},
+        { name: 'MET', molecule: 'C11', halflife: '20.38', color: 'yellow'}
+    ])
+
